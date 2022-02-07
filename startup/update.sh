57af69ac46db95395eea982943d5feccd9f73c0c
Jack Terwilliger <jack.f.terwilliger@gmail.com>
2022-02-07 02:02:57 +0000

    fewer updates

--- startup/update.sh
+++ startup/update.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 git checkout main
-checkout=$?
-git pull
-pull=$?
-cd .. && make clean && make build
-build=$?
-if [ -f /mnt/log.txt ]; then
-	if [ $checkout -eq 0 ]; then
-		echo "successfully checkout latest version" >> /mnt/log.txt
-	else
-		echo "failed to checkout latest version" >> /mnt/log.txt
+t=`git branch -r --sort=committerdate | tail -1`
+a=`git rev-parse $t`
+b=`git rev-parse HEAD`
+if [ "$a" != "$b" ]; then
+	checkout=$?
+	git pull
+	pull=$?
+	cd .. && make clean && make build
+	build=$?
+	if [ -f /mnt/log.txt ]; then
+		if [ $checkout -eq 0 ]; then
+			echo "successfully checkout latest version" >> /mnt/log.txt
+		else
+			echo "failed to checkout latest version" >> /mnt/log.txt
+		fi
+		if [ $pull -eq 0 ]; then
+			echo "successfully download latest version" >> /mnt/log.txt
+		else
+			echo "failed to download latest version" >> /mnt/log.txt
+		fi
+		if [ $build -eq 0 ]; then
+			echo "successfully build latest version" >> /mnt/log.txt
+		else
+			echo "failed build latest version" >> /mnt/log.txt
+		fi
 	fi
-	if [ $pull -eq 0 ]; then
-		echo "successfully download latest version" >> /mnt/log.txt
-	else
-		echo "failed to download latest version" >> /mnt/log.txt
-	fi
-	if [ $build -eq 0 ]; then
-		echo "successfully build latest version" >> /mnt/log.txt
-	else
-		echo "failed build latest version" >> /mnt/log.txt
-	fi
+else
+	echo "up to date"
 fi
