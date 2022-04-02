18517f3d6bfa30e2c7ddbb26e253201262875239
Jack Terwilliger <jack.f.terwilliger@gmail.com>
2022-04-02 12:03:18 -0700

    copy over credentials (#12)
    * credential change & logging

--- startup/copy_config.sh
+++ startup/copy_config.sh
@@ -16,6 +16,28 @@ if [ -e $drive ]; then
 	else
 		echo "json config is improperly formatted. Contact jack@theycantalk.org" >> /mnt/log.txt
 	fi
+	# copy over other files
+	if [ -f /mnt/.env ]; then
+		cp /mnt/.env /srv/beholder_configuration/.env
+		if [ -f /mnt/log.txt ]; then
+			echo "copied over .env" >> /mnt/log.txt
+		fi
+	else
+		if [ -f /mnt/log.txt ]; then
+			echo "no such file .env found" >> /mnt/log.txt
+		fi
+	fi
+	# copy over other files
+	if [ -f /mnt/credentials.ini ]; then
+		cp /mnt/credentials.ini /srv/beholder_configuration/credentials.ini
+		if [ -f /mnt/log.txt ]; then
+			echo "copied over credentials.ini" >> /mnt/log.txt
+		fi
+	else
+		if [ -f /mnt/log.txt ]; then
+			echo "no such file credentials.ini found" >> /mnt/log.txt
+		fi
+	fi
 	# copy it over and if it works, we might as well try to boot the internet
 	cp /mnt/config.json /srv/beholder_configuration/config.json
 	if [ $? -eq 0 ]; then
